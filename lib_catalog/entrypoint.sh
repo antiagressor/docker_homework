d59ac811ba13b5eee84fbd7cfe29802b6f6828dc
Aleks <aleks@aleks.local>
2022-01-18 10:06:18 +0300

    refactoring

--- lib_catalog/entrypoint.sh
+++ lib_catalog/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python manage.py makemigrations && python manage.py migrate && python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" && python manage.py runserver 0.0.0.0:8000
-
+file="initiate"
+if [ ! -f "$file" ];
+then
+   python manage.py migrate && python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" && python manage.py runserver 0.0.0.0:8000 \ 
+   touch initiate
+else
+   python manage.py runserver 0.0.0.0:8000
+fi
